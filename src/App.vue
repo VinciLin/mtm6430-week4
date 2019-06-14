827e3ae95c4dab4d6305d9fa4b62187adf4507c3
Fangyuan Lin <lin00146@algonquinlive.com>
2019-06-14 10:51:22 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- 3. add click and v-if = true into below -->
+    <el-button type="primary" @click = "openDiag = true">Welcome</el-button>
+    <!-- 1. bind a proporty -->
+    <!-- add".sync", el- is a component, add sync to update in -->
+    <el-dialog :visible.sync = "openDiag">
+ 
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -11,12 +25,20 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    simba
+  },
+  data:function(){
+    return {
+      sendTreat: "Raw Hide",
+      // 2. set false
+      openDiag: false,
+    }
+  },
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/simba.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>HELLO {{name}}</h2>
+    <ul>
+      <li>Age: {{age}}</li>
+      <li>Color: {{color}}</li>
+      <li>{{ treat }}</li>
+    </ul>
+  </div>
+</template>  
+<script>
+export default {
+  props: ["treat"],
+  data: function() {
+    // what value want to receive from the parent in App.vue,"treat" become Proporty, call above in the <template>
+
+    return {
+      name: "Simba",
+      age: 6,
+      color: "black"
+    };
+  }
+};
+</script>  
+<style >
+h2 {
+  color: coral;
+}
+</style>
